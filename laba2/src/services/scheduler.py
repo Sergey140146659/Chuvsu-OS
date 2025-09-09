112b575fe624601b1c831a30845c6bc5e6a26c9a
Sergei Shcherbakov <shchsergey@10.215.140.222-vpn.dhcp.yndx.net>
2025-09-09 23:38:40 +0300

    laba2 complete

---
+++ laba2/src/services/scheduler.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+from collections import deque
+from ..core.process import Process, ProcessState
+
+class Scheduler:
+    def __init__(self):
+        self.ready_queue: deque[Process] = deque()
+
+    def add_process(self, process: Process):
+        if process.state == ProcessState.NEW:
+            process.state = ProcessState.READY
+            self.ready_queue.append(process)
+
+    def get_next_process(self) -> Optional[Process]:
+        if not self.ready_queue:
+            return None
+        return self.ready_queue[0]
+
+    def remove_process(self, pid: int):
+        process_to_remove = None
+        for p in self.ready_queue:
+            if p.pid == pid:
+                process_to_remove = p
+                break
+
+        if process_to_remove:
+            self.ready_queue.remove(process_to_remove)
+
+    @property
+    def has_ready_processes(self) -> bool:
+        return len(self.ready_queue) > 0
